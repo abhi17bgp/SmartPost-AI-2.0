ad2d73b64cffbf554bb1a9348aaadf77abee797d
Abhishek Anand <iabhishekbgp21@gmail.com>
2026-04-01 08:42:22 +0530

    replace local link to deployed link

--- client/src/context/WorkspaceContext.jsx
+++ client/src/context/WorkspaceContext.jsx
@@ -98,7 +98,7 @@ export const WorkspaceProvider = ({ children }) => {
       return;
     }
 
-    const newSocket = io('http://localhost:5000', {
+    const newSocket = io('https://smartpost-ai-2-0.onrender.com', {
       withCredentials: true
     });
     setSocket(newSocket);
